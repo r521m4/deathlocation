5e74da0fa127e6ebe23d09c031bf0ac7613bca3b
hinata5021 <76790613+hinata5021@users.noreply.github.com>
2023-04-24 22:41:52 +0900

    Initial commit

---
+++ src/main/java/com/github/r521m4/deathlocation/Deathlocation.kt
@@ -0,0 +1,13 @@
+package com.github.r521m4.deathlocation
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Deathlocation : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
